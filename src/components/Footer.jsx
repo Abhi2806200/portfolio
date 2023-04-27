9260ed729ec96d8f6ecb8f909b2bde2976924765
Abhishek Agnihotri <abhishekagnihotri@antiersolutions.com>
2023-04-27 17:32:34 +0530

    new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return(
         <div class="text-center p-6 bg-black footer">
-        <span class="text-white">&copy; 2023 Copyright - A.A </span>
+        <span class="text-white">&copy; 2023 Copyright - </span>
         <a class="link-underline text-white font-bold" href="https:##">Abhishek Agnihotri</a>
       </div>
     )
